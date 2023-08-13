413308a592fbe7d9107e62d05cbbdb58265839d7
woahmedosmaa <mohamedmedo9932@gmail.com>
2023-08-14 02:37:06 +0300

    Done Newest Books Method

--- lib/features/home/presentation/manager/newest_books_cubit/newest_book_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_book_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:book_app/features/home/data/models/BookModel.dart';
+import 'package:book_app/features/home/data/repo/home_repo.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -8,4 +9,15 @@ class NewestBookCubit extends Cubit<NewestBookState> {
   NewestBookCubit() : super(NewestBookInitial());
 
   static NewestBookCubit get(context) => NewestBookCubit();
+
+  Future<void> fetchNewestBook() async {
+    emit(NewestBookLoadingState());
+    HomeRepo? homeRepo;
+    var result = await homeRepo!.fetchNewestBooks();
+    result.fold((failure) {
+      emit(NewestBookFailureState(failure.errorMessage));
+    }, (books) {
+      emit(NewestBookSuccessState(books));
+    });
+  }
 }
