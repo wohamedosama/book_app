4d6d249f8bb9f3033d6394843a29f9ff35b5d650
woahmedosmaa <mohamedmedo9932@gmail.com>
2023-08-14 01:54:47 +0300

    Crate Feature Books Method

--- lib/features/home/presentation/manager/feature_books_cubit/feature_book_cubit.dart
+++ lib/features/home/presentation/manager/feature_books_cubit/feature_book_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/features/home/data/repo/home_repo.dart';
 import 'package:book_app/features/home/presentation/manager/feature_books_cubit/feature_book_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -5,4 +6,15 @@ class FeatureBookCubit extends Cubit<FeatureBookState> {
   FeatureBookCubit() : super(FeatureBookInitial());
 
   static FeatureBookCubit get(context) => FeatureBookCubit();
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeatureBooksLoadingState());
+    HomeRepo? homeRepo;
+    var result = await homeRepo!.fetchFeatureBooks();
+    result.fold((failure) {
+      emit(FeatureBooksFailureState(failure.errorMessage));
+    }, (books) {
+      emit(FeatureBooksSuccessState(books));
+    });
+  }
 }
